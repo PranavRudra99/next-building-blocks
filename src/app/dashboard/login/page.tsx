aa1aa0c718792850d6841b54e8504a108f82a9bb
Pranav <pranavrudra1999@gmail.com>
2023-11-16 00:16:19 -0500

    Login page WIP

--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { Button, Input, Grid, Card } from '@mantine/core'
+import { Button, TextInput, Grid, Card } from '@mantine/core'
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 type LoginInputs = {
-    username: string
+    email: string
     password: string
 }
 
@@ -11,30 +11,28 @@ const Login = (): JSX.Element => {
     const {
         register,
         handleSubmit,
-        setValue
-    } = useForm<LoginInputs>()
+        reset,
+        formState: {errors, isDirty, isValid, touchedFields}
+    } = useForm<LoginInputs>({mode: "onChange", defaultValues: {email: '', password: ''}})
 
     const handleLogin: SubmitHandler<LoginInputs> = (data) => {
-        setValue('username', '')
-        setValue('password', '')
+        console.log(data)
+        reset()
     }
 
     return (
         <Card shadow='sm' padding='xl'>
+            <h2 className='login-header center mb-1'>Welcome back!</h2>
             <form onSubmit={handleSubmit(handleLogin)}>
                 <Grid className='pl-1 pr-1'>
                     <Grid.Col>
-                        <Input.Wrapper label='Email'>
-                            <Input placeholder='Enter email' {...register('username', { required: true })}></Input>
-                        </Input.Wrapper>
+                            <TextInput label='Email' placeholder='Enter email' {...register('email', { required: true, pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/ })} required error={errors.email?.type} />
                     </Grid.Col>
                     <Grid.Col>
-                        <Input.Wrapper label='Password'>
-                            <Input type='password' placeholder='Enter password' {...register('password', { required: true })}></Input>
-                        </Input.Wrapper>
+                        <TextInput type='password' label='Password' placeholder='Enter password' {...register('password', { required: true })} required error={errors.password?.type} />
                     </Grid.Col>
                     <Grid.Col className='center mt-1'>
-                        <Button fullWidth type='submit'>Submit</Button>
+                        <Button fullWidth type='submit' disabled={!isValid || !isDirty }>Submit</Button>
                     </Grid.Col>
                 </Grid>
             </form>
