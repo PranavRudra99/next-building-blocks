c054f1e2cd59037ef1200e79d62e635342505dca
Pranav <pranavrudra1999@gmail.com>
2023-11-17 16:48:05 -0500

    Content refactor to JSONs

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,39 +1,42 @@
 'use client'
-import Navigation from "@/components/Navigation"
-import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from "@mantine/core"
+import Navigation from '@/components/Navigation'
+import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from '@mantine/core'
 import { IconPuzzleFilled, IconMoon, IconSun } from '@tabler/icons-react'
+import content from '@/content/dashboard.json'
+import {useColorScheme} from '@mantine/hooks'
 
 const DashboardLayout = ({
     children,
 }: {
     children: React.ReactNode
 }) => {
+    const colorScheme = useColorScheme();
     const { setColorScheme } = useMantineColorScheme();
-    const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true });
+    const computedColorScheme = useComputedColorScheme(colorScheme, { getInitialValueInEffect: true });
     return (
-        <div className="d-flex flex-col full-height">
-            <div className="d-grid truly-center">
+        <div className='d-flex flex-col full-height'>
+            <div className='d-grid truly-center'>
                 <div>
                     <Navigation />
                 </div>
-                <div className="vertical-center">
+                <div className='vertical-center'>
                     <IconPuzzleFilled size={24} />
                     <div>
-                        <h1 className="margin-none">Building Blocks</h1>
+                        <h1 className='margin-none'>{content.primaryHeader}</h1>
                     </div>
                 </div>
-                <div className="vertical-center p-5p ml-auto">
+                <div className='vertical-center p-5p ml-auto'>
                     <ActionIcon
                         onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
                         variant='default'
                         size='lg'
-                        aria-label='Toggle color switch'
+                        aria-label={content.toggleADA}
                     >
                         {computedColorScheme === 'light' ? <IconMoon/>:<IconSun/>}
                     </ActionIcon>
                 </div>
             </div>
-            <div className="center flex-grow-1">
+            <div className='center flex-grow-1'>
                 {children}
             </div>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <MantineProvider defaultColorScheme='auto'>
           {children}
