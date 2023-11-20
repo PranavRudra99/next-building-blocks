3ca4dd1d7c4fb962cd43f5028b7827c5491d361b
Pranav <pranavrudra1999@gmail.com>
2023-11-20 13:59:44 -0500

    Minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <MantineProvider defaultColorScheme='auto'>
+        <MantineProvider defaultColorScheme='dark'>
           {children}
         </MantineProvider>
       </body>
