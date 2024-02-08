dd83128eb61da97e1f107ddabb59113efb94c2e4
Pranav <pranavrudra1999@gmail.com>
2024-02-07 20:48:10 -0500

    Stepper Page WIP

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -2,14 +2,20 @@ import { stepperColors } from "@/utilities/types";
 import { useState } from "react";
 
 const Stepper = ({ items, bgColors }: { items: string[], bgColors: stepperColors }) => {
+    
     const [selectedStep, setSelectedStep] = useState<number>(0);
+    
+    const handleClick = (index: number) => {
+        setSelectedStep(index)
+    }
+    
     return (
         <div className="horizontal-center">
             {items.map((item, index) => {
                 return (
-                    <div key={index} style={index === selectedStep ? { background: bgColors.active } : { background: bgColors.inactive }}>
+                    <div key={index} style={index === selectedStep ? { background: bgColors.active } : { background: bgColors.inactive }} onClick={() => {handleClick(index)}}>
                         <div className="margin-all-1">
-                            <div>
+                            <div style={index === selectedStep ? { color: "white" } : { color: "black" }}>
                                 {item}
                             </div>
                         </div>
